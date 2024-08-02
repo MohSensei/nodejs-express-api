1fe4c67bf149d52a24d1bab336e3cccee7507fbe
Mohammed <74876877+MohSensei@users.noreply.github.com>
2024-08-02 16:39:41 -0400

    Added the information like name and name of the company to invest in, when this information is passed in the route.rest file it will show in the server once the person asks for it

--- routes/investors.js
+++ routes/investors.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const Investor = require('../models/investor')
+const investor = require('../models/investor')
 
 // Getting all
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async (req, res) => {
+    try {
+        const investors = await investor.find()
+        res.json(investors)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 // Getting one
 router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
 // Creating one
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const investor = new Investor({
+        name: req.body.name,
+        investingToCompany:
+        req.body.investingToCompany
+    })
 
+    try {
+        newInvestor = await investor.save()
+        res.status(201).json(newInvestor)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
 })
 // Updating one
 router.patch('/', (req, res) => {
